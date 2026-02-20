208334e613fbf4476124f0f154198024ee2abdbf
Libraum <wmchoward@internode.on.net>
2026-02-20 22:01:50 +1030

    BucketItem adjustment

--- src/main/java/net/libraum/platypusmod/items/custom/PlatypusBucketItem.java
+++ src/main/java/net/libraum/platypusmod/items/custom/PlatypusBucketItem.java
@@ -1,7 +1,6 @@
 package net.libraum.platypusmod.items.custom;
 
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.EntityBucketItem;
@@ -9,13 +8,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundEvent;
 
+import java.util.Objects;
+
 public class PlatypusBucketItem extends EntityBucketItem {
     public PlatypusBucketItem(EntityType<?> type, Fluid fluid, SoundEvent emptyingSound, Settings settings) {
         super(type, fluid, emptyingSound, settings);
     }
 
     public static float getVariant(ItemStack stack) {
-        NbtCompound nbt = stack.get(DataComponentTypes.BUCKET_ENTITY_DATA).copyNbt();
-        return ((float) nbt.getInt("Variant") / 4);
+        NbtCompound tag = Objects.requireNonNull(stack.get(DataComponentTypes.BUCKET_ENTITY_DATA)).copyNbt();
+        return ((float) tag.getInt("Variant") / 4);
     }
 }
