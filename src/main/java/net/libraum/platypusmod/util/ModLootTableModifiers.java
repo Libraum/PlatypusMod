e4300f5dd364328f8a916b87b8034410ae6425d5
Libraum <wmchoward@internode.on.net>
2026-02-21 17:10:49 +1030

    Loot Tables fix

--- src/main/java/net/libraum/platypusmod/util/ModLootTableModifiers.java
+++ src/main/java/net/libraum/platypusmod/util/ModLootTableModifiers.java
@@ -3,20 +3,29 @@ package net.libraum.platypusmod.util;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.libraum.platypusmod.items.ModItems;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.LocationCheckLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.entity.LocationPredicate;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 
 public class ModLootTableModifiers {
-    private static final Identifier Village_Fisher_Chest_ID =
+
+    private static final Identifier VILLAGE_FISHER_CHEST_ID =
             Identifier.of("minecraft", "chests/village/village_fisher");
 
     public static void modifyLootTables() {
-                LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
-            if(Village_Fisher_Chest_ID.equals(key.getValue())) {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+
+            if(VILLAGE_FISHER_CHEST_ID.equals(key.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(0.33f))
