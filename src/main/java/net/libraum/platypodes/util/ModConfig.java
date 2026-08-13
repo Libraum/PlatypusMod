18f695b7b3c461a7439b7abdb21c943d63dc0da1
Libraum <wmchoward@internode.on.net>
2026-08-13 11:22:12 +0930

    1.20.1 More config options + Seagrass drops moved to code

--- src/main/java/net/libraum/platypodes/util/ModConfig.java
+++ src/main/java/net/libraum/platypodes/util/ModConfig.java
@@ -11,15 +11,22 @@ public class ModConfig extends MidnightConfig {
 //    @Comment(centered = true) public static Comment attributesRestart;
 
     /** Platypus Spawning */
-    @Comment(name = "Platypus Spawning:", centered = true) public static Comment spawning;
-    @Entry(name = "Enable Spawning") public static boolean enableSpawns = true;
-    @Entry(name = "Spawn During") public static SpawnDuring spawnDuring = SpawnDuring.DAWNANDDUSK;
+    @Comment(centered = true) public static Comment spawning;
+    @Entry public static boolean enableSpawns = true;
+    @Entry public static SpawnDuring spawnDuring = SpawnDuring.DAWNANDDUSK;
     public enum SpawnDuring {
         DAWNANDDUSK, NIGHT, ALLDAY
     }
-    @Entry(name = "Rare Variant Chance", min = 1) public static int rareVariantChance = 1200;
-    @Entry(name = "Spawn Weight", min = 1) public static int spawnWeight = 1;
-    @Entry(name = "Minimum Group Size", min = 0) public static int minSpawnGroup = 1;
-    @Entry(name = "Maximum Group Size", min = 1) public static int maxSpawnGroup = 1;
+    @Entry(min = 1) public static int rareVariantChance = 1200;
+    @Entry(min = 1) public static int spawnWeight = 1;
+    @Entry(min = 0) public static int minSpawnGroup = 1;
+    @Entry(min = 1) public static int maxSpawnGroup = 1;
 
+    /** Yabby Sources **/
+    @Comment(centered = true) public static Comment yabbySources;
+    @Entry(isSlider = true, min = 0f, max = 1f, precision = 100) public static float seagrassDrops = 0.05f;
+    @Entry public static boolean fisherChests = true;
+    //    @Entry public static boolean fisherTrades = true;
+//    @Entry public static boolean wanderingTrades = true;
+    @Comment(centered = true) public static Comment lootPoolRestart;
 }
--- src/main/java/net/libraum/platypodes/util/ModLootTableModifiers.java
+++ src/main/java/net/libraum/platypodes/util/ModLootTableModifiers.java
@@ -2,11 +2,15 @@ package net.libraum.platypodes.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.libraum.platypodes.items.ModItems;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.predicates.LocationCheck;
+import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.predicates.InvertedLootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.MatchTool;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.minecraft.resources.ResourceLocation;
@@ -14,10 +18,14 @@ import net.minecraft.resources.ResourceLocation;
 public class ModLootTableModifiers {
     private static final ResourceLocation Village_Fisher_Chest_ID =
             new ResourceLocation("minecraft", "chests/village/village_fisher");
+    private static final ResourceLocation SEAGRASS =
+            ResourceLocation.tryBuild("minecraft", "blocks/seagrass");
+    private static final ResourceLocation TALL_SEAGRASS =
+            ResourceLocation.tryBuild("minecraft", "blocks/tall_seagrass");
 
     public static void modifyLootTables() {
                 LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-                    if(Village_Fisher_Chest_ID.equals(id)) {
+                    if(Village_Fisher_Chest_ID.equals(id) && ModConfig.fisherChests) {
                         LootPool.Builder poolBuilder = LootPool.lootPool()
                                 .setRolls(ConstantValue.exactly(1))
                                 .when(LootItemRandomChanceCondition.randomChance(0.33f))
@@ -25,6 +33,29 @@ public class ModLootTableModifiers {
                                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0f, 3.0f)).build());
                         tableBuilder.pool(poolBuilder.build());
                      }
+
+                    if(SEAGRASS.equals(id)) {
+                        LootPool.Builder poolBuilder = LootPool.lootPool()
+                                .setRolls(ConstantValue.exactly(1))
+                                .when(LootItemRandomChanceCondition.randomChance(ModConfig.seagrassDrops)) /* Default: 5% */
+                                .when(InvertedLootItemCondition.invert(MatchTool.toolMatches(ItemPredicate.Builder.item().of(Items.SHEARS))))
+                                .when(ExplosionCondition.survivesExplosion())
+                                .add(LootItem.lootTableItem(ModItems.YABBY))
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0f, 2.0f)).build());
+                        tableBuilder.pool(poolBuilder.build());
+                    }
+
+                    if(TALL_SEAGRASS.equals(id)) {
+                        LootPool.Builder poolBuilder = LootPool.lootPool()
+                                .setRolls(ConstantValue.exactly(1))
+                                .when(LootItemRandomChanceCondition.randomChance(ModConfig.seagrassDrops)) /* Default: 5% */
+                                .when(InvertedLootItemCondition.invert(MatchTool.toolMatches(ItemPredicate.Builder.item().of(Items.SHEARS))))
+                                .when(ExplosionCondition.survivesExplosion())
+                                .add(LootItem.lootTableItem(ModItems.YABBY))
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0f, 1.0f)).build());
+                        tableBuilder.pool(poolBuilder.build());
+                    }
+
                 });
     }
 }
